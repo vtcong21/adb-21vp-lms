f04bb287c95a5668af1da46b1994911e3b60422a
vtcong21 <vtcong21@vp.fitus.edu.vn>
2024-08-14 15:29:19 +0700

    done backend learner

--- server/controllers/learner.js
+++ server/controllers/learner.js
@@ -4,7 +4,7 @@ import getPool from "../utils/database";
 
 export const addCourseToCart = async (req, res) => {
   try {
-    // code here
+
     const { learnerId, courseId } = req.body;
     //console.log(req.body);
 
@@ -29,7 +29,7 @@ export const addCourseToCart = async (req, res) => {
 
 export const removeCourseFromCart = async (req, res) => {
   try {
-    // code here
+
     const { learnerId, courseId } = req.body;
     //console.log(req.body);
 
@@ -62,7 +62,7 @@ export const getCartDetails = async (req, res) => {
     }
 
     if (!learnerId) {
-      return res.status(400).json({ message: "learnerId are required" });
+      return res.status(400).json({ message: "learnerId is required" });
     }
     const jsonResult = await pool.executeSP('sp_LN_GetCartDetails', { learnerId });
 
@@ -106,7 +106,7 @@ export const viewOrders = async (req, res) => {
     }
 
     if (!learnerId) {
-      return res.status(400).json({ message: "learnerId are required" });
+      return res.status(400).json({ message: "learnerId is required" });
     }
     const jsonResult = await pool.executeSP('sp_LN_ViewOrders', { learnerId });
 
@@ -141,11 +141,25 @@ export const viewOrderDetails = async (req, res) => {
   }
 };
 
-export const instructorPayoutSettings = async (req, res) => {
+export const completeLesson = async (req, res) => {
   try {
-    // code here
+    const { learnerId, courseId, sectionId, lessonId } = req.body;
+
+    const pool = getPool('LMS');
+
+    if (!pool) {
+      return res.status(500).json({ message: "Database pool is not available" });
+    }
+
+    if (!learnerId || !courseId || !sectionId || !lessonId) {
+      return res.status(400).json({ message: "learnerId, courseId, sectionId, lessonId are required" });
+    }
+    await pool.executeSP('sp_LN_CompleteLesson', { learnerId, courseId, sectionId, lessonId });
+
+    return res.status(200).json({ message: "Paid successfully" });
 
   } catch (err) {
-    console.log("stripe payout settings login link err => , err");
+    console.log("ERR ", err);
+    return res.status(400).json({ error: err.message });
   }
 };
